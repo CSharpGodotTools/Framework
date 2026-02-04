9a82892d996cb98e0cf1e22e2ce7958a3d6654a7
Valk <sebastianbelle074@protonmail.com>
2026-02-04 00:47:45 -0600

    Fix various issues with PacketGen source gen
    - PacketRegistry class is now only generated when the [PacketRegistry] attribute is used.
    - PacketRegistryAttribute takes in an optional parameter where you are able to specify the packet opcode size (default is byte but can be changed to for example ushort or uint)
    - PacketRegistryAttribute uses the class name and namespace name it's specified in for the name of the PacketRegistry class and the namespace it resides in
    - Source gen performance has improved significantly across the board.
    - Opcodes in PacketRegistry are now generated in an deterministic order (before the order was random determined by ClientPacket / ServerPacket class names and the number of these classes)

--- Netcode/Packet/PacketInfo.cs
+++
@@ -1,7 +0,0 @@
-namespace Framework.Netcode;
-
-public class PacketInfo<T>
-{
-    public byte Opcode { get; set; }
-    public T Instance { get; set; }
-}
---
+++ Netcode/Packet/PacketRegistryAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Framework.Netcode;
+
+[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class PacketRegistryAttribute : System.Attribute
+{
+    public System.Type OpcodeType { get; }
+
+    public PacketRegistryAttribute()
+    {
+        OpcodeType = typeof(byte);
+    }
+
+    public PacketRegistryAttribute(System.Type opcodeType)
+    {
+        OpcodeType = opcodeType;
+    }
+}
