bca5a43384c679ddabdfd46451a5c865f36b38fc
Valk <sebastianbelle074@protonmail.com>
2026-02-09 19:52:24 -0600

    Remove redundant initializer

--- Netcode/Examples/TopDown/Server/Packets/SPacketPlayerPositions.cs
+++ Netcode/Examples/TopDown/Server/Packets/SPacketPlayerPositions.cs
@@ -5,5 +5,5 @@ namespace Framework.Netcode.Examples.Topdown;
 
 public partial class SPacketPlayerPositions : ServerPacket
 {
-    public Dictionary<uint, Vector2> Positions { get; set; } = [];
+    public Dictionary<uint, Vector2> Positions { get; set; }
 }
